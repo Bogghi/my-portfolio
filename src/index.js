9a4af8caf66d635ff2d68f7d7ea0be1cf7225570
matteo.borghi <matteosimoneborghi@gmail.com>
2021-05-22 11:50:29 +0200

    first commit

---
+++ src/index.js
@@ -0,0 +1,222 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+function Sep() {
+    return (
+        <div className="sep"></div>
+    )
+}
+
+function Heading (props) {
+    return (
+        <div className="title">
+            <h1>{props.value}</h1>
+        </div>
+    )
+}
+
+function QuickDesc (props) {
+    return (
+        <div className="desc">
+            {props.value}
+        </div>
+    )
+}
+
+class Work extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            Resume: '#',
+            Auto1: '#',
+            Quantyca: '#',
+        };
+        this.title = 'work';
+    }
+
+    render() {
+        const jobs = this.state;
+        const a = [];
+        for (const k in jobs) {
+            if (Object.hasOwnProperty.call(jobs, k)) {
+                const e = jobs[k];
+                const jl = Object.keys(jobs).length;
+                const current = Object.keys(jobs)[Object.keys(jobs).length-(jl-1)];
+                a.push(
+                    <div key={k} title={k === current ? 'current occupation' : ''}>
+                        <a
+                            key = {k+'_a'}
+                            href={e}
+                            rel="noopener noreferrer">
+                            {k}
+                        </a>
+                        <span key = {k+'_span'}>{k === current ? '🧘🏼' : ''}</span>
+                    </div>
+                )
+            }
+        }
+        return (
+            <div className="work">
+                <h6>{this.title}</h6>
+                <div className="container">{a}</div>
+            </div>
+        )
+    }
+}
+
+class Article extends React.Component {
+    constructor(props){
+        super(props);
+        //type is ither _blank (external link) or _self (local link)
+        this.state = {
+            'Digitalize your life': {
+                type: '_blank',
+                link: 'https://medium.com/@BogghiIsThere/digitalize-your-life-trust-me-its-worth-it-f3386a7521dd'
+            }
+        };
+        this.title = 'Article';
+    }
+
+    render() {
+        const a = this.state;
+        const link = [];
+
+        for (const k in a) {
+            if (Object.hasOwnProperty.call(a, k)) {
+                const e = a[k];
+                link.push(
+                    <a
+                        key={k+'_a'}
+                        target={e['type']}
+                        rel="noopener noreferrer"
+                        href={e['link']}>
+                        {k}
+                    </a>
+                )
+            }
+        }
+        return (
+            <div>
+                <h6>{this.title}</h6>
+                <div className="article">{link}</div>
+            </div>
+        )
+    }
+}
+
+class Link extends React.Component {
+    constructor(props){
+        super(props);
+        this.state  = {
+            Github: 'https://github.com/Bogghi',
+            Twitter: 'https://twitter.com/BogghiIsThere',
+            Linkedin: 'https://www.linkedin.com/in/matteosimoneborghi/'
+        }
+    }
+    
+    render(){
+        const a = [];
+        for (const key in this.state) {
+            if (Object.hasOwnProperty.call(this.state, key)) {
+                const e = this.state[key];
+                const separator = Object.keys(this.state)[Object.keys(this.state).length-1];
+                a.push(
+                    <div key={key} className="s-link">
+                        <a
+                            key={key+'_a'} 
+                            target="_blank"
+                            rel="noopener noreferrer" 
+                            href={e}>{key}
+                        </a> 
+                        {separator !== key ? <span key={key+'_span'}>•</span> : ""}
+                    </div>
+                )       
+            }
+        }
+        return (
+            <div className="link">{a}</div>
+        )
+    }
+}
+
+
+class LeftContainer extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            title: 'Matteo Borghi',
+            desc: 'Perseverance is my talent and long life learning are my main life driver. Currently based in Lissone (MB). Tech, Self Improvment, Fantasy Book are among my interest'
+        }
+    }
+
+    render() {
+        return (
+            <div className="left-container">
+                <Heading value={this.state.title}/>
+                <Sep />
+                <QuickDesc value={this.state.desc}/>
+                <Link />
+                <Article />
+                <Work />
+            </div>
+        )
+    }
+}
+
+class RightContainer extends React.Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            imgOne : '/img/placeholder.jpg',
+            imgTwo : '/img/placeholder_2.jpg'
+        }
+    }
+    
+    render(){
+        const imgs = this.state;
+        const e = [];
+        for (const k in imgs) {
+            if (Object.hasOwnProperty.call(imgs, k)) {
+                const img = imgs[k];
+                e.push(
+                    <img src={img} alt="img"/>
+                )              
+            }
+        }
+
+        return (
+            <div className="right-container">
+                {e}
+            </div>
+        )
+    }
+}
+
+class Container extends React.Component {
+    render(){
+        return (
+            <div className="container">
+                <LeftContainer />
+                <RightContainer />
+            </div>
+        );
+    }
+}
+
+class Main extends React.Component {
+    render() {
+        return (
+            <div className="main">
+                <Container />
+            </div>
+        );
+    } 
+}
+
+//==========================================
+
+ReactDOM.render(
+<Main />,
+document.getElementById('root')
+);
