001fd8f83e97b6c4e594549cf60c2c0d62cf4ee0
matteo.borghi <matteosimoneborghi@gmail.com>
2021-07-10 22:38:29 +0200

    added responsive menu part 2

--- src/components/home-dropdown.js
+++ src/components/home-dropdown.js
@@ -4,14 +4,21 @@ import DropdownList from './dropdown-list';
 class HomeDropdown extends React.Component {
     constructor(props){
         super(props);
+        this.state = {
+            color: 'none'
+        }
     }
 
     render(){
         return(
             <div className="home-dropdown">
-                <i class="fas fa-bars"></i>
+                <i class="fas fa-bars" onTouchStart={this.changeColor}></i>
             </div>
         )
+    }
+
+    changeColor(){
+        
     }
 }
 
